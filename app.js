8e5d1cc1618a8d4c93aa6b7d9a0240e83f1f599e
Javier Flores <javier.flores.tecnologico@gmail.com>
2024-07-17 14:16:25 -0600

    Actualiza app.js

--- app.js
+++ app.js
@@ -1,27 +1,52 @@
 function encriptar(){
     let textoEncriptar = document.querySelector("textarea").value;
+    let mostrar = document.getElementById("div-right");
+    let regresar = document.getElementById("div-principal-mensaje");
+    let copiar = document.getElementById("copiar");
+    let imagen = document.getElementById("muñeco");
     
     if(textoEncriptar.trim() !== ""){
         let nuevoMensaje = textoEncriptar.replace(/e/g,"enter").replace(/i/g,"imes").replace(/a/g,"ai").replace(/o/g,"ober").replace(/u/g,"ufat");
-        let mostrar = document.getElementById("div-right");
 
-        mostrar.style.paddingLeft = "20px";
-        mostrar.innerHTML = nuevoMensaje;
+        mostrar.style.paddingLeft = "2px";
+        mostrar.textContent = nuevoMensaje;
+
+        copiar.style.display = "";
+        imagen.style.display = "none";
+
     }else{
         alert("Por favor escribe algo en el area del texto");
+        mostrar.innerHTML = '<img src="images/Muñeco.png" alt="Descripción de la imagen">' +  
+        '<p id="mensaje-negritas">Ningun mensaje fue encontrado</p>' + 
+        '<p id="mensaje">Ingresa el mensaje que deseas encriptar o desencriptar</p>';
+        copiar.style.display = "none";
+        imagen.style.display = "block";
     }
 }
 
 function desencriptar(){
     let textoEncriptar = document.querySelector("textarea").value;
+    let mostrar = document.getElementById("div-right");
+    let regresar = document.getElementById("div-principal-mensaje");
+    let copiar = document.getElementById("copiar");
+    let imagen = document.getElementById("muñeco");
     
     if(textoEncriptar.trim() !== ""){
         let nuevoMensaje = textoEncriptar.replace(/enter/g,"e").replace(/imes/g,"i").replace(/ai/g,"a").replace(/ober/g,"o").replace(/ufat/g,"u");
         let mostrar = document.getElementById("div-right");
 
-        mostrar.style.paddingLeft = "20px";
+        mostrar.style.paddingLeft = "2px";
         mostrar.innerHTML = nuevoMensaje;
+
+        copiar.style.display = "";
+        imagen.style.display = "none";
+
     }else{
         alert("Por favor escribe algo en el area del texto");
+        mostrar.innerHTML = '<img src="images/Muñeco.png" alt="Descripción de la imagen">' +  
+        '<p id="mensaje-negritas">Ningun mensaje fue encontrado</p>' + 
+        '<p id="mensaje">Ingresa el mensaje que deseas encriptar o desencriptar</p>';
+        copiar.style.display = "none";
+        imagen.style.display = "block";
     }
 }
