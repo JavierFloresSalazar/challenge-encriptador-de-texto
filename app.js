9153debc94ceeef66fd1506b8ff7f0fd27517617
Javier Flores <javier.flores.tecnologico@gmail.com>
2024-07-17 14:46:04 -0600

    actualiza app.js

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 function encriptar(){
     let textoEncriptar = document.querySelector("textarea").value;
     let mostrar = document.getElementById("div-right");
-    let regresar = document.getElementById("div-principal-mensaje");
     let copiar = document.getElementById("copiar");
     let imagen = document.getElementById("muñeco");
-    
-    if(textoEncriptar.trim() !== ""){
+    var soloMinusculas = /^[a-z\s]*$/.test(textoEncriptar);
+
+    if(!soloMinusculas){
+        alert("solo debes ingresar minusculas sin acentos");
+        let mitext = document.querySelector("textarea");
+            mitext.value = "";
+        mostrar.innerHTML = '<img src="images/Muñeco.png" alt="Descripción de la imagen">' +  
+        '<p id="mensaje-negritas">Ningun mensaje fue encontrado</p>' + 
+        '<p id="mensaje">Ingresa el mensaje que deseas encriptar o desencriptar</p>';
+    } else if(textoEncriptar.trim() !== ""){
         let nuevoMensaje = textoEncriptar.replace(/e/g,"enter").replace(/i/g,"imes").replace(/a/g,"ai").replace(/o/g,"ober").replace(/u/g,"ufat");
 
         mostrar.style.paddingLeft = "2px";
@@ -30,8 +37,16 @@ function desencriptar(){
     let regresar = document.getElementById("div-principal-mensaje");
     let copiar = document.getElementById("copiar");
     let imagen = document.getElementById("muñeco");
+    var soloMinusculas = /^[a-z\s]*$/.test(textoEncriptar);
     
-    if(textoEncriptar.trim() !== ""){
+    if(!soloMinusculas){
+        alert("solo debes ingresar minusculas sin acentos");
+        let mitext = document.querySelector("textarea");
+            mitext.value = "";
+        mostrar.innerHTML = '<img src="images/Muñeco.png" alt="Descripción de la imagen">' +  
+        '<p id="mensaje-negritas">Ningun mensaje fue encontrado</p>' + 
+        '<p id="mensaje">Ingresa el mensaje que deseas encriptar o desencriptar</p>';
+    } else if(textoEncriptar.trim() !== ""){
         let nuevoMensaje = textoEncriptar.replace(/enter/g,"e").replace(/imes/g,"i").replace(/ai/g,"a").replace(/ober/g,"o").replace(/ufat/g,"u");
         let mostrar = document.getElementById("div-right");
 
